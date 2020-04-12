86c75aa53ab2dd99e1c0af042b0fc74f7cc4b3cd
Federico Ferreyra Lombardi <federicoferreyra2@gmail.com>
2020-04-12 20:05:15 -0300

    Modificiacion de producto por forntend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,6 +9,7 @@ class App extends Component {
     this.state = {
       productos: [],
       nuevoProductoModal: false,
+      editarProductoModal: false,
       productoData: {
         nombre: '',
         precio: 0,
@@ -24,15 +25,25 @@ class App extends Component {
   }
 
 componentWillMount() {
-    axios.get('http://localhost:8080/productos')
-        .then((res) => {
-          this.setState({productos : res.data})
-        })     
+    this.actualizarProductos();
 }
 
 toggleNuevoProductoModel(){
   this.setState({
     nuevoProductoModal: ! this.state.nuevoProductoModal
+  });
+}
+
+actualizarProductos(){
+  axios.get('http://localhost:8080/productos')
+        .then((res) => {
+          this.setState({productos : res.data})
+        })     
+}
+
+toggleEditarProductoModel(){
+  this.setState({
+    editarProductoModal: ! this.state.editarProductoModal
   });
 }
 
@@ -49,16 +60,41 @@ agregarProducto() {
   });
 }
 
+editarProducto(){
+  let {nombre, precio, cantidad} = this.state.editProductoData;
+  axios.put('http://localhost:8080/producto/' + this.state.editProductoData.id, {
+    nombre, precio, cantidad
+  }).then((res) => {
+    this.actualizarProductos();
+    this.setState({ editarProductoModal: false });
+  });
+}
+
+//cuando se abre la pantalla modal se cargan los datos en los input desde aquí
+editarProductoModalData(producto){
+  this.setState({
+    editProductoData: {
+      id: producto._id,
+      nombre: producto.nombre,
+      precio: producto.precio,
+      cantidad: producto.cantidad
+    },
+    editarProductoModal: !this.state.editarProductoModal
+  });
+}
+
 render(){ 
   let listProductos = this.state.productos.map((prod) => {
     return(
       <tr>
-        <td>{prod.id}</td>
+        <td>{prod._id}</td>
         <td>{prod.nombre}</td>
         <td>{prod.precio}</td>
         <td>{prod.cantidad}</td>
         <td>
-          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full mr-2">
+          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full mr-2"
+                  onClick={this.editarProductoModalData.bind(this, prod)}
+          >
             Editar
           </button>
           <button className="bg-red-700 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
@@ -72,40 +108,8 @@ render(){
   return (
     <div>
 
-      <Button color="primary" onClick={this.toggleNuevoProductoModel.bind(this)}>Agregar Producto</Button>
-      <Modal isOpen={this.state.nuevoProductoModal} toggle={this.toggleNuevoProductoModel.bind(this)}>
-        <ModalHeader className="bg-indigo-500" toggle={this.toggleNuevoProductoModel.bind(this)}>Agregar Producto</ModalHeader>
-        <ModalBody className="bg-indigo-500">
-          <FormGroup>
-            <Label for="Nombre">Nombre: </Label>
-            <Input id="nombre" value={this.state.productoData.nombre} onChange={(event) =>  {
-              let { productoData } = this.state;
-              productoData.nombre = event.target.value;
-              this.setState({productoData})
-            }}/>
-          </FormGroup>
-          <FormGroup>
-            <Label for="Precio">Precio: </Label>
-            <Input id="precio" value={this.state.productoData.precio} onChange={(event) =>  {
-              let { productoData } = this.state;
-              productoData.precio = event.target.value;
-              this.setState({productoData})
-            }}/>
-          </FormGroup>
-          <FormGroup>
-            <Label for="Cantidad">Cantidad: </Label>
-            <Input id="cantidad" value={this.state.productoData.cantidad} onChange={(event) =>  {
-              let { productoData } = this.state;
-              productoData.cantidad = event.target.value;
-              this.setState({productoData})
-            }}/>
-          </FormGroup>
-        </ModalBody>
-        <ModalFooter className="bg-indigo-500">
-          <button className="bg-blue-700 hover:bg-blue-900 text-white font-bold py-2 px-4 rounded-full" onClick={this.agregarProducto.bind(this)}>Agregar</button>
-          <button className="bg-gray-700 hover:bg-black text-white font-bold py-2 px-4 rounded-full" onClick={this.toggleNuevoProductoModel.bind(this)}>Cancel</button>
-        </ModalFooter>
-      </Modal>
+      {this.renderNuevoProductoModal()}
+      {this.renderEditarProductoModal()}
 
       <table className="table">
         <thead>
@@ -125,6 +129,88 @@ render(){
        
     );
   }
+
+  renderNuevoProductoModal() {
+    return(
+      <div>
+      <Button color="primary" onClick={this.toggleNuevoProductoModel.bind(this)}>Agregar Producto</Button>
+        <Modal isOpen={this.state.nuevoProductoModal} toggle={this.toggleNuevoProductoModel.bind(this)}>
+          <ModalHeader className="bg-indigo-500" toggle={this.toggleNuevoProductoModel.bind(this)}>Agregar Producto</ModalHeader>
+          <ModalBody className="bg-indigo-500">
+            <FormGroup>
+              <Label for="Nombre">Nombre: </Label>
+              <Input id="nombre" value={this.state.productoData.nombre} onChange={(event) =>  {
+                let { productoData } = this.state;
+                productoData.nombre = event.target.value;
+                this.setState({productoData})
+              }}/>
+            </FormGroup>
+            <FormGroup>
+              <Label for="Precio">Precio: </Label>
+              <Input id="precio" value={this.state.productoData.precio} onChange={(event) =>  {
+                let { productoData } = this.state;
+                productoData.precio = event.target.value;
+                this.setState({productoData})
+              }}/>
+            </FormGroup>
+            <FormGroup>
+              <Label for="Cantidad">Cantidad: </Label>
+              <Input id="cantidad" value={this.state.productoData.cantidad} onChange={(event) =>  {
+                let { productoData } = this.state;
+                productoData.cantidad = event.target.value;
+                this.setState({productoData})
+              }}/>
+            </FormGroup>
+          </ModalBody>
+          <ModalFooter className="bg-indigo-500">
+            <button className="bg-blue-700 hover:bg-blue-900 text-white font-bold py-2 px-4 rounded-full" onClick={this.agregarProducto.bind(this)}>Agregar</button>
+            <button className="bg-gray-700 hover:bg-black text-white font-bold py-2 px-4 rounded-full" onClick={this.toggleNuevoProductoModel.bind(this)}>Cancel</button>
+          </ModalFooter>
+        </Modal>
+        </div>
+    );
+  }
+
+  renderEditarProductoModal() {
+    return(
+      <div>
+        <Modal isOpen={this.state.editarProductoModal} toggle={this.toggleEditarProductoModel.bind(this)}>
+          <ModalHeader className="bg-indigo-500" toggle={this.toggleEditarProductoModel.bind(this)}>Editar Producto</ModalHeader>
+          <ModalBody className="bg-indigo-500">
+            <FormGroup>
+              <Label for="Nombre">Nombre: </Label>
+              <Input id="nombre" value={this.state.editProductoData.nombre} onChange={(event) =>  {
+                let { editProductoData } = this.state;
+                editProductoData.nombre = event.target.value;
+                this.setState({editProductoData})
+              }}/>
+            </FormGroup>
+            <FormGroup>
+              <Label for="Precio">Precio: </Label>
+              <Input id="precio" value={this.state.editProductoData.precio} onChange={(event) =>  {
+                 let { editProductoData } = this.state;
+                 editProductoData.precio = event.target.value;
+                 this.setState({editProductoData})
+              }}/>
+            </FormGroup>
+            <FormGroup>
+              <Label for="Cantidad">Cantidad: </Label>
+              <Input id="cantidad" value={this.state.editProductoData.cantidad} onChange={(event) =>  {
+                 let { editProductoData } = this.state;
+                 editProductoData.cantidad = event.target.value;
+                 this.setState({editProductoData})
+              }}/>
+            </FormGroup>
+          </ModalBody>
+          <ModalFooter className="bg-indigo-500">
+            <button className="bg-blue-700 hover:bg-blue-900 text-white font-bold py-2 px-4 rounded-full" onClick={this.editarProducto.bind(this)}>Editar</button>
+            <button className="bg-gray-700 hover:bg-black text-white font-bold py-2 px-4 rounded-full" onClick={this.toggleEditarProductoModel.bind(this)}>Cancel</button>
+          </ModalFooter>
+        </Modal>
+        </div>
+    );
+  }
 }
+
 
 export default App;
