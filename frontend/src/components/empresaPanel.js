088d95f689468afaca8f185855eeba5b416b1227
Federico Ferreyra Lombardi <federicoferreyra2@gmail.com>
2020-05-13 21:56:49 -0300

    Se muestra información de la empresa de un usuario en la página panel de empresa

--- frontend/src/components/empresaPanel.js
+++ frontend/src/components/empresaPanel.js
@@ -1,19 +1,31 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class EmpresaPanel extends Component{
 
     constructor(props){
         super(props);
-        
+        this.state = { empresa: false };
     }
 
+    componentDidMount(){
+        this.consultarEmpresa();    
+    }
+
+    consultarEmpresa(){
+        //Acá tengo que pasar el usuario una vez que tenga el login
+        axios.get('http://localhost:8080/usuario/5ebb37c7fb5efb104fc5b6cb/empresa')
+        .then((res) => {
+          this.setState({empresa : res.data});
+        });
+    }
     
     render(){
         return(
-            <div>
+            <div className="container mt-2">
                 <div>
                     <h4>Empresa</h4>
-                    
+                    <p>{ this.state.empresa.nombre }</p>
                 </div>
             </div>        
         )
