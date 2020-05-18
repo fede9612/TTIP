fc7aae61757583f337b6f420589dc342816b8607
Federico Ferreyra Lombardi <federicoferreyra2@gmail.com>
2020-05-18 11:31:53 -0300

    Agregando ventana modal para cargar sucursales, comunicación entre componente hijo al padre

--- frontend/src/components/empresaPanel.js
+++ frontend/src/components/empresaPanel.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
 import axios from 'axios';
 import LocalRow from './localRow';
+import LocalModal from './localModal';
 
 class EmpresaPanel extends Component{
 
@@ -9,12 +10,18 @@ class EmpresaPanel extends Component{
         super(props);
         this.state = { 
             empresa: false,
-            locales: [] 
+            locales: [],
+            localModal: false
         };
+        this.handlerLocalModal = this.handlerLocalModal.bind(this);
     }
 
     componentDidMount(){
         this.consultarEmpresa(); 
+    }
+
+    handlerLocalModal(){
+        this.setState({localModal: !this.state.localModal})
     }
 
     consultarEmpresa(){
@@ -31,9 +38,13 @@ class EmpresaPanel extends Component{
                 <LocalRow local={local} />
             );
         });
+        let localModal;
+        if(this.state.localModal){
+             localModal = <LocalModal handlerClick={this.handlerLocalModal}/>
+             
+        }
 
         return(
-            
             <div className="container mt-2">
                 <div class="flex flex-wrap">
                     <div class="w-full lg:w-1/4 ">
@@ -42,7 +53,14 @@ class EmpresaPanel extends Component{
                         <p>{ this.state.empresa.nombre }</p>
                     </div>
                     <div class="w-full lg:w-3/4">
-                        <h4>Sucursales</h4>
+                        <div className="flex">
+                            <h4>Sucursales</h4>
+                            <button className="bg-green-500 hover:bg-green-700 text-white font-bold px-2 ml-2 h-7 border-b-4 border-l-4 border-t-4 border-r-4 rounded-full"
+                                    onClick={this.handlerLocalModal}>
+                                Agregar                
+                            {localModal}
+                            </button>
+                        </div>
                         <hr className="w-4/5 mt-1"></hr>
                         {localesList}
                     </div>
