53ac8cdb87c040ebca1be2714ed4c69ceb2f9ee1
Federico Ferreyra Lombardi <federicoferreyra2@gmail.com>
2020-05-18 17:37:30 -0300

    Mejora visual en el listado de locales de empresaPanel

--- frontend/src/components/localRow.js
+++ frontend/src/components/localRow.js
@@ -11,10 +11,21 @@ class LocalRow extends Component{
 
     render(){
         return(
-            <div>
-                <p>{this.state.local.nombre}</p>
-                <span>Carrito de compra</span> <span>Productos</span>
-            </div>
+            <div className="mt-2 w-full lg:w-4/5">
+                <div className="border-r border-b border-l border-t border-gray-700 lg:border-t lg:border-gray-700 rounded-b lg:rounded-b-none lg:rounded-r p-2 flex flex-col justify-between leading-normal">
+                  <div>
+                    <div className="text-gray-900 font-bold text-xl mb-2">{this.state.local.nombre}</div>
+                  </div>
+                    <spam className="text-gray-700 text-base">Pedidos pendientes: <spam>{this.state.local.carritosDePedido.length}</spam></spam>
+                    <spam className="text-gray-700 text-base">Productos en stock: <spam>{this.state.local.productos.length}</spam></spam>
+                  <div className="flex items-center">
+                    
+                    <div className="text-xs">
+                    <spam className="text-gray-600">Dirección: {this.state.local.direccion} </spam>                 
+                    </div>
+                  </div>
+                </div>
+              </div>
         )
     }
 }
