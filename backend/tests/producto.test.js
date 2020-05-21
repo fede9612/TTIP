7bc9b57450ad6f0810bcb73c53928324bc8d36b8
Federico Ferreyra Lombardi <federicoferreyra2@gmail.com>
2020-05-21 12:08:16 -0300

    Configuración de test unitarios, cargar producto test

---
+++ backend/tests/producto.test.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+const Producto = require('../src/models/producto');
+const productoData = { nombre: 'Cocacola', precio: 20, cantidad: 50, local: "5ec2e7fe1ce10a11cdd876f9" }
+
+describe('Producto Model Test', () => {
+
+    // It's just so easy to connect to the MongoDB Memory Server 
+    // By using mongoose.connect
+    beforeAll(async () => {
+        await mongoose.connect("mongodb://localhost/anydirec", { useNewUrlParser: true, useCreateIndex: true }, (err) => {
+            if (err) {
+                console.error(err);
+                process.exit(1);
+            }
+        });
+    });
+
+    it('create & save user successfully', async () => {
+        const validProducto = new Producto(productoData);
+        const savedProducto = await validProducto.save();
+        // Object Id should be defined when successfully saved to MongoDB.
+        expect(savedProducto._id).toBeDefined();
+        expect(savedProducto.nombre).toBe(productoData.nombre);
+        expect(savedProducto.precio).toBe(productoData.precio);
+        expect(savedProducto.cantidad).toBe(productoData.cantidad);
+        // expect(savedProducto.local).toBe(productoData.local);
+    });
+
+    
+})
