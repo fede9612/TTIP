9a4d4c504c9e248729acd7a48d642fecca5dc80c
Federico Ferreyra Lombardi <federicoferreyra2@gmail.com>
2020-05-21 22:08:37 -0300

    Agregando local al producto en el test de agregar un producto

--- backend/tests/producto.test.js
+++ backend/tests/producto.test.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 const Producto = require('../src/models/producto');
-const productoData = { nombre: 'Cocacola', precio: 20, cantidad: 50, local: "5ec2e7fe1ce10a11cdd876f9" }
+const Local = require('../src/models/local');
+const productoData = { nombre: 'Cocacola', precio: 20, cantidad: 50}
+const localData = { nombre:"Local de ropa", direccion: "Av. Alem 1323"}
 
 describe('Producto Model Test', () => {
 
@@ -14,14 +16,17 @@ describe('Producto Model Test', () => {
     });
 
     it('Guardar Producto correctamente', async () => {
+        const validLocal = new Local(localData);
+        await validLocal.save();
         const validProducto = new Producto(productoData);
+        validProducto.local = validLocal;
         const savedProducto = await validProducto.save();
         // Object Id should be defined when successfully saved to MongoDB.
         expect(savedProducto._id).toBeDefined();
         expect(savedProducto.nombre).toBe(productoData.nombre);
         expect(savedProducto.precio).toBe(productoData.precio);
         expect(savedProducto.cantidad).toBe(productoData.cantidad);
-        // expect(savedProducto.local).toBe(productoData.local);
+        expect(savedProducto.local.nombre).toBe(localData.nombre);
     });
 
     
