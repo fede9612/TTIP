24494634c896f72e1b043a641561336eaefe3b37
Federico Ferreyra Lombardi <federicoferreyra2@gmail.com>
2020-05-21 21:26:06 -0300

    Actualizacion de conexión a la base de datos en los test

--- backend/tests/producto.test.js
+++ backend/tests/producto.test.js
@@ -1,21 +1,26 @@
-const mongoose = require('mongoose');
+const {MongoClient} = require('mongodb');
 const Producto = require('../src/models/producto');
 const productoData = { nombre: 'Cocacola', precio: 20, cantidad: 50, local: "5ec2e7fe1ce10a11cdd876f9" }
 
 describe('Producto Model Test', () => {
 
     beforeAll(async () => {
-        await mongoose.connect("mongodb://localhost/anydirec", { useNewUrlParser: true, useCreateIndex: true }, (err) => {
-            if (err) {
-                console.error(err);
-                process.exit(1);
-            }
+        connection = await MongoClient.connect("mongodb://localhost/", {
+          useNewUrlParser: true,
+          useUnifiedTopology: true
         });
-    });
+        db = await connection.db("anydirec");
+      });
+    
+      afterAll(async () => {
+        await connection.close();
+        await db.close();
+      });
 
     it('Guardar Producto correctamente', async () => {
-        const validProducto = new Producto(productoData);
-        const savedProducto = await validProducto.save();
+        const productos = db.collection('productos');
+        await productos.insertOne(productoData);
+        const savedProducto = await productos.findOne(productoData);
         // Object Id should be defined when successfully saved to MongoDB.
         expect(savedProducto._id).toBeDefined();
         expect(savedProducto.nombre).toBe(productoData.nombre);
