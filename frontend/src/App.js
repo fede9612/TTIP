1c477bdc7cf37e24c402b58803716bda9d613248
Federico Ferreyra Lombardi <federicoferreyra2@gmail.com>
2020-05-25 23:13:39 -0300

    Actualizaciones de dependencia auth0-js en package.json y limpieza de imports en el código

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,7 @@
 import React, { Component } from 'react';
 import './styles/app.css';
 import Navegacion from './components/navegacion';
-import auth0Client from './Auth';
-import history from "./utils/history";
-import { Router, Route, Switch, withRouter } from "react-router-dom";
-import PrivateRoute from './components/privateRoute';
-import EmpresaPanel from './components/empresaPanel';
+import { withRouter } from "react-router-dom";
 
 class App extends Component {
   
