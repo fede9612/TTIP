e3c80c277d2b5b773f9f54f910fce16a104e948e
Federico Ferreyra Lombardi <federicoferreyra2@gmail.com>
2020-05-30 21:22:44 -0300

    integración de twilio para enviar mensajes de whatsapp

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,17 @@ const express = require('express');
 const bodyParser = require("body-parser");
 var cors = require('cors');
 const mongoose = require('mongoose');
+const accountSid = 'AC4bbaef748e471bfc7613ea45ecb3751c';
+const authToken = '1a0521ce48361207e11dde1dd11c41a5';
+const client = require('twilio')(accountSid, authToken);
+
+// client.messages
+//       .create({
+//          from: 'whatsapp:+14155238886',
+//          body: 'Hello there!',
+//          to: 'whatsapp:+5492478446733'
+//        })
+//       .then(message => console.log(message.sid));
 
 mongoose.set("useNewUrlParser", true);
 mongoose.set("useUnifiedTopology", true);
