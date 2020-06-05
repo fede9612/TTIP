3723958b396ebfcc7091cc9997c0df25e2ec2eb2
Federico Ferreyra Lombardi <federicoferreyra2@gmail.com>
2020-06-05 10:34:33 -0300

    Switch para entrar a la página de una empresa o a la página anydirec

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,29 @@
 import React, { Component } from 'react';
 import './styles/app.css';
 import Navegacion from './components/navegacion';
-import { withRouter } from "react-router-dom";
+import { withRouter, Route, Switch } from "react-router-dom";
+import EmpresaPage from '../src/components/empresaPage';
 
 class App extends Component {
   
   render(){
+    {console.log(this.props.location.pathname)}
     return (
       <div>
-        <Navegacion />
+        <Switch>
+          <Route path="/empresa/:id" component={PageEmpresa}/>
+          <Route path="/" component={Navegacion}/>
+        </Switch>
       </div>
     );
   }
 }
+
+const PageEmpresa = () => (
+  <div>
+    <Route path="/empresa/:id" component={EmpresaPage}/>
+  </div>
+);
 
 
 export default withRouter(App);
