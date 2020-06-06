1ad40fb5a7f8aa6b3574dda500ff7a2972cc2030
Federico Ferreyra Lombardi <federicoferreyra2@gmail.com>
2020-06-05 23:21:12 -0300

    borrado de log

--- backend/database/src/controller/usuario.js
+++ backend/database/src/controller/usuario.js
@@ -36,7 +36,6 @@ module.exports = {
         const {nickname} = req.params;
         const usuario = await Usuario.findOne({'mail': nickname});
         const pedido = await Carrito.findOne({'usuarioDelPedido._id': usuario._id, "pendiente": true}).populate('local');
-        console.log(pedido)
         return res.send(pedido);
     },
 
