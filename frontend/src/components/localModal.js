2d496eb295d868e1e1f003e935ba03b99d9fe00b
Federico Ferreyra Lombardi <federicoferreyra2@gmail.com>
2020-06-18 14:23:54 -0300

    Se cargan los datos de lat y long al crear un nuevo local, según lo que se elija en el mapa

--- frontend/src/components/localModal.js
+++ frontend/src/components/localModal.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
-import { Modal, ModalHeader, ModalBody, ModalFooter, Input, Label } from 'reactstrap';
+import { Modal, ModalHeader, ModalBody, ModalFooter, Input, Label, Row, Col } from 'reactstrap';
 import axios from 'axios';
+import Map from "./local/map";
 
 class LocalModal extends Component{
 
@@ -12,13 +13,16 @@ class LocalModal extends Component{
                 direccion: "",
                 mail: "",
                 productos: [],
-                carritosDePedidos: []
+                carritosDePedidos: [],
+                latitud: "",
+                longitud: ""
             },
             empresa: props.empresa,
             modal: true
         };
         this.toggle = this.toggle.bind(this);
         this.agregarLocal = this.agregarLocal.bind(this);
+        this.cargarLatYLong = this.cargarLatYLong.bind(this);
     }
 
     toggle(){
@@ -31,6 +35,18 @@ class LocalModal extends Component{
         axios.post('http://localhost:8080/empresa/'+ this.state.empresa._id +'/local', this.state.local)
         .then((res) => {this.props.agregar(res.data)})
         .then(this.toggle());
+    }
+
+    cargarLatYLong(lat, long){
+        // var {local} = this.state;
+        // local.latitud = lat;
+        // local.longitud = long;
+        // this.setState({local: local});
+        this.state.local.latitud = lat;
+        this.state.local.longitud = long;
+        console.log("Latitud: ", this.state.local.latitud);
+        console.log("Longitud: ", this.state.local.longitud);
+        
     }
 
     render(){
@@ -57,6 +73,17 @@ class LocalModal extends Component{
                             local.mail = event.target.value;
                             this.setState({local})
                         }}/>
+                        <Row className="mt-3 mb-24">
+                            <Col>
+                                <Map
+                                    google={this.props.google}
+                                    center={{lat: 18.5204, lng: 73.8567}}
+                                    height='300px'
+                                    zoom={15}
+                                    cargarLatYLong={this.cargarLatYLong}
+                                />
+                            </Col>
+                        </Row>
                     </ModalBody>
                     <ModalFooter className="bg-teal-500">
                     <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded-full" onClick={this.agregarLocal}>Agregar</button>
