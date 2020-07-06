6a6507bfed5156f57672a15664a0ac2056011abb
Federico Ferreyra Lombardi <federicoferreyra2@gmail.com>
2020-07-06 14:00:20 -0300

    Configuración para recargar cualquier página y que no lance el error cannot get /nombreDelComponente

--- frontend/server.js
+++ frontend/server.js
@@ -12,5 +12,8 @@ app.use(function(req, res, next) {
 });
 app.use(cors());
 app.use('/', express.static(path.join(__dirname, 'build')));
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+  });
 
 app.listen(port, () => console.log("Listening on Port", port));
