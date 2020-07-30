d614f207e74f7bdfe8044c8224700d0ad7dc0f23
Federico Ferreyra Lombardi <federicoferreyra2@gmail.com>
2020-07-30 10:31:23 -0300

    Logo

--- frontend/src/components/navegacion.js
+++ frontend/src/components/navegacion.js
@@ -77,7 +77,7 @@ class Navegacion extends Component {
                 <div>    
                     <nav class="flex items-center justify-between flex-wrap bg-teal-500 p-1">
                     <div class="flex items-center flex-shrink-0 text-white m-2">
-                        <svg class="fill-current h-8 w-8 mr-2" viewBox="0 0 54 54" xmlns="http://www.w3.org/2000/svg"><path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z"/></svg>
+                        <strong class="font-semibold mr-2 text-justify text-xl border-t border-b border-white">WL</strong> 
                         <span class="font-semibold text-xl tracking-tight">WebLocales</span>
                     </div>
                     <div class="block lg:hidden">
