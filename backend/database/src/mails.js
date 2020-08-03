6bd215bf3d1fb8e893a34cbad58b1e612196f391
Federico Ferreyra Lombardi <federicoferreyra2@gmail.com>
2020-08-03 13:00:15 -0300

    weblocales por anydirec, en la URL y el footer

--- backend/database/src/mails.js
+++ backend/database/src/mails.js
@@ -21,7 +21,7 @@ module.exports = {
         let _text = "<h2>Tiene un nuevo pedido<h2>"
                     + "<span>Usted tiene un nuevo pedido en su local</span>"
         // send mail with defined transport object
-        var messageHtml = '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd"><div><div style="width:100%;background-color:#00BFA6;padding:2%" align="center"><span style="color:black;font-size:2rem">Tiene un nuevo pedido</span></div><div style="width:100%;padding-top:6%;padding-bottom:6%" align="center"><span style="font-size:1rem">Por favor revise sus pedidos en nuestra página web <a href="http://localhost:3000/">Anydirec</a></span></div><div style="width:100%;background-color:#00BFA6;padding:4%" align="center"><span style="color:black;font-size:1rem">Copyright© 2020 Anydirec</span></div></div>'
+        var messageHtml = '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd"><div><div style="width:100%;background-color:#00BFA6;padding:2%" align="center"><span style="color:black;font-size:2rem">Tiene un nuevo pedido</span></div><div style="width:100%;padding-top:6%;padding-bottom:6%" align="center"><span style="font-size:1rem">Por favor revise sus pedidos en nuestra página web <a href="http://weblocales.com">WebLocales</a></span></div><div style="width:100%;background-color:#00BFA6;padding:4%" align="center"><span style="color:black;font-size:1rem">Copyright© 2020 WebLocales</span></div></div>'
         let info = await transporter.sendMail({
           from: '"WebLocales" <notificaciones@weblocales.com>', // sender address
           to: mail, // list of receivers
--- frontend/src/components/mails/pedidoMail.js
+++ frontend/src/components/mails/pedidoMail.js
@@ -14,11 +14,11 @@ export function pedidoListo(){
                     </div>
                     <div style={{width:"100%", paddingTop:"6%", paddingBottom:"6%"}} align="center">
                         <Image style={{width:"35%", marginBottom:"4px"}} src="https://imgs-anydirec.s3-sa-east-1.amazonaws.com/happy_notification.png"></Image>
-                        <span style={{fontSize:"1rem"}}>Por favor revise sus pedidos listos del carrito en nuestra página web <a href={process.env.REACT_APP_URL}>Anydirec</a></span>
+                        <span style={{fontSize:"1rem"}}>Por favor revise sus pedidos listos del carrito en nuestra página web <a href={process.env.REACT_APP_URL}>WebLocales</a></span>
                     </div>
                     <div style={{width:"100%", backgroundColor:"#00BFA6", padding:"4%"}} align="center">
                         <span style={{color:"black", fontSize:"1rem"}}>
-                            Copyright© 2020 Anydirec
+                            Copyright© 2020 WebLocales
                         </span>
                     </div>
                 </div>
