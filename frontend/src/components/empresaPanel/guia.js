782a725a616e15af8f2dee6aa57054d6d3b279bc
Federico Ferreyra Lombardi <federicoferreyra2@gmail.com>
2020-08-10 18:42:27 -0300

    Estilo

--- frontend/src/components/empresaPanel/guia.js
+++ frontend/src/components/empresaPanel/guia.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Row, Col, Container } from 'reactstrap';
+import { Row, Col, Container, ListGroupItem } from 'reactstrap';
 
 class Guia extends Component {
 
@@ -8,16 +8,21 @@ class Guia extends Component {
             <Container>
                 <Row className="text-center mt-4">
                     <Col>
-                        <span className="text-5xl font-home-page">Configuraciones iniciales</span>
-                        <div class="embed-responsive embed-responsive-16by9">
-                            <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/1-WqH-wwApY?rel=0" 
-                                    allowfullscreen="allowfullscreen"
-                                    mozallowfullscreen="mozallowfullscreen" 
-                                    msallowfullscreen="msallowfullscreen" 
-                                    oallowfullscreen="oallowfullscreen" 
-                                    webkitallowfullscreen="webkitallowfullscreen">    
-                            </iframe>
-                        </div>
+                        <ListGroupItem>
+                            <span className="text-5xl font-home-page">Configuraciones iniciales</span>
+                            <div className="flex justify-center -mt-3 mb-3">
+                                <hr className="w-8/12 bg-green-400"></hr>
+                            </div>
+                            <div class="embed-responsive embed-responsive-16by9">
+                                <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/1-WqH-wwApY?rel=0" 
+                                        allowfullscreen="allowfullscreen"
+                                        mozallowfullscreen="mozallowfullscreen" 
+                                        msallowfullscreen="msallowfullscreen" 
+                                        oallowfullscreen="oallowfullscreen" 
+                                        webkitallowfullscreen="webkitallowfullscreen">    
+                                </iframe>
+                            </div>
+                        </ListGroupItem>
                     </Col>    
                 </Row>          
             </Container>
