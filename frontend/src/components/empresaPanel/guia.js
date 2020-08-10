2a0dcd4271fc855590643323c9098bc25dc7d6fd
Federico Ferreyra Lombardi <federicoferreyra2@gmail.com>
2020-08-10 19:55:21 -0300

    Video de agregar productos

--- frontend/src/components/empresaPanel/guia.js
+++ frontend/src/components/empresaPanel/guia.js
@@ -24,7 +24,26 @@ class Guia extends Component {
                             </div>
                         </ListGroupItem>
                     </Col>    
-                </Row>          
+                </Row>
+                <Row className="text-center mt-4 mb-5">
+                    <Col>
+                        <ListGroupItem>
+                            <span className="text-5xl font-home-page">Agrega productos a tu página web</span>
+                            <div className="flex justify-center -mt-3 mb-3">
+                                <hr className="w-8/12 bg-green-400"></hr>
+                            </div>
+                            <div class="embed-responsive embed-responsive-16by9">
+                                <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/wJzDc6yuB7Y?rel=0" 
+                                        allowfullscreen="allowfullscreen"
+                                        mozallowfullscreen="mozallowfullscreen" 
+                                        msallowfullscreen="msallowfullscreen" 
+                                        oallowfullscreen="oallowfullscreen" 
+                                        webkitallowfullscreen="webkitallowfullscreen">    
+                                </iframe>
+                            </div>
+                        </ListGroupItem>
+                    </Col>    
+                </Row>      
             </Container>
         );
     }
