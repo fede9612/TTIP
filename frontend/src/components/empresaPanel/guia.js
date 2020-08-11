47a4f33a15edd8053ab20c86b60991320bd38af0
Federico Ferreyra Lombardi <federicoferreyra2@gmail.com>
2020-08-11 19:27:31 -0300

    Comparte tu página web y productos

--- frontend/src/components/empresaPanel/guia.js
+++ frontend/src/components/empresaPanel/guia.js
@@ -25,7 +25,7 @@ class Guia extends Component {
                         </ListGroupItem>
                     </Col>    
                 </Row>
-                <Row className="text-center mt-4 mb-5">
+                <Row className="text-center mt-4">
                     <Col>
                         <ListGroupItem>
                             <span className="text-5xl font-home-page">Agrega productos a tu página web</span>
@@ -34,6 +34,25 @@ class Guia extends Component {
                             </div>
                             <div class="embed-responsive embed-responsive-16by9">
                                 <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/wJzDc6yuB7Y?rel=0" 
+                                        allowfullscreen="allowfullscreen"
+                                        mozallowfullscreen="mozallowfullscreen" 
+                                        msallowfullscreen="msallowfullscreen" 
+                                        oallowfullscreen="oallowfullscreen" 
+                                        webkitallowfullscreen="webkitallowfullscreen">    
+                                </iframe>
+                            </div>
+                        </ListGroupItem>
+                    </Col>    
+                </Row>
+                <Row className="text-center mt-4 mb-5">
+                    <Col>
+                        <ListGroupItem>
+                            <span className="text-5xl font-home-page">Comparte tu página web y productos</span>
+                            <div className="flex justify-center -mt-3 mb-3">
+                                <hr className="w-8/12 bg-green-400"></hr>
+                            </div>
+                            <div class="embed-responsive embed-responsive-16by9">
+                                <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/sBBxCkLaKKE?rel=0" 
                                         allowfullscreen="allowfullscreen"
                                         mozallowfullscreen="mozallowfullscreen" 
                                         msallowfullscreen="msallowfullscreen" 
